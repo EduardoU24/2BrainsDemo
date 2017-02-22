6e03427038935e27bcdb3dd807ecc79f65488f25
EduardoU24 <eduardo.u24@gmail.com>
2017-02-22 16:00:22 -0300

    Initial files and folder setup

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+/*
+* Configuration 
+*/ 
+RestangularProvider.setBaseUrl('https://jsonplaceholder.typicode.com');
+angular.module('twobrainsdemo', ['restangular']);
+
+/*
+* Logic 
+*/
+angular.module('twobrainsdemo').controller('MainCtrl', function($scope, Restangular) {
+	
+});
