cc48d3450c94524fa8d239096ecd1c9922a79f38
EduardoU24 <eduardo.u24@gmail.com>
2017-02-22 19:39:36 -0300

    main frame added +m
    +Get All Post 
    +Calculate Fibonacci
    +Get Post from those Fibonacci 
    +Get Post until "this" Fibonacci
    
    todo:
    - Style
    - Post Details
    - Post "post": add
    - Post "put/patch": modify
    - Post "delete"
    - same as above for users and other stuff form server.

--- js/app.js
+++ js/app.js
@@ -5,15 +5,91 @@
 'use strict';
 angular.module('twobrainsdemo', ['restangular', 'ngRoute'])
 .config(function ($routeProvider, RestangularProvider) {
-	RestangularProvider.setBaseUrl('https://jsonplaceholder.typicode.com');
+	RestangularProvider.setBaseUrl('https://jsonplaceholder.typicode.com/');
 
 	$routeProvider.when('/', {
-		controller: 'MainCtrl'
+		controller: 'MainCtrl',
+		templateUrl: 'main.html'
+	}).when('/post/:id', {
+		controller: 'PostCtrl',
+		templateUrl: 'post.html'
 	});
 })
 /*
 * Logic 
 */
-.controller('MainCtrl', function($scope, Restangular) {
-	$scope.posts = Restangular.all('posts');
-});
+.controller('MainCtrl', function($scope, Restangular, $location) {
+	//#region PostData
+	$scope.getAllPosts = function() {
+		$scope.posts = Restangular.all('posts').getList().$object;
+		console.log($scope.posts);
+	};
+
+	$scope.getCommentsFromPost = function( id ) {
+		// hardcode TODO: give comment to right post
+		$scope.posts[0].comments = Restangular.all('posts/'+ id +'/comments').getList().$object;
+	}
+
+	$scope.getFibonacciPosts = function( _num ) {
+		var _sequence = $scope.getFibonacciSequence(_num);
+		console.log("sequence: ", _sequence);
+		$scope.posts = Restangular.all('posts' + _sequence).getList().$object;
+	};
+
+
+	$scope.getFibonacciPostsUntil = function( _num ) {
+		var _sequence = $scope.getFibonacciSequenceUntil(_num);
+		console.log("sequence: ", _sequence);
+		$scope.posts = Restangular.all('posts' + _sequence).getList().$object;
+	};
+	//#endregion PostData
+
+	// Returns X fibonacci numbers
+	$scope.getFibonacciSequence = function( _num ) {
+		var _sequence = [];
+		for (var i = 0; i <= _num; i += 1) {
+			var fib = $scope.fibonacci(i);
+			_sequence.push(fib);
+		}
+		return $scope.parseFibSequenceForRest(_sequence);
+	};
+
+	// Returns a fibonacci sequence UNTIL given number is reached.
+	$scope.getFibonacciSequenceUntil = function( _num ) {
+		var _sequence = [];
+		for (var i = 0; i <= _num; i += 1) {
+			var fib = $scope.fibonacci(i);
+			if(fib > _num)
+				return $scope.parseFibSequenceForRest(_sequence);
+
+			_sequence.push(fib);
+		}
+		return $scope.parseFibSequenceForRest(_sequence);
+	};
+
+	// Turns the array into a string for the http to request
+	$scope.parseFibSequenceForRest = function( _sequence ) {
+		return "?id=" + _sequence.join('&id=');
+	};
+
+	// Calculate fibonacci value 
+	$scope.fibonacci = function fibonacci(_num) {
+		// this should be  ' _num <= 1 ' but I only want the last "one" since I don't like duplicated posts.
+ 		if (_num <= 0) return 1;
+		return $scope.fibonacci( _num - 1) + $scope.fibonacci(_num - 2);
+	};
+
+	$scope.GoToPostDetails = function( id ) {
+		$location.path( "post/" + id );
+	};
+})
+
+.controller('PostCtrl', function($scope, $routeParams, Restangular) {
+	$scope.post = Restangular.one('posts', $routeParams.id).get().$object;
+	$scope.getComments = function() {
+		// hardcode TODO: give comment to right post
+		$scope.post.comments = Restangular.all('posts/'+ $scope.post.id +'/comments').getList().$object;
+	}
+})
+
+;
